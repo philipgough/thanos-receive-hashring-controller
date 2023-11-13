62aff6b224203f58840d2c1f35664c6ace5f6da6
Philip Gough <philip.p.gough@gmail.com>
2023-11-13 15:11:37 +0000

    Support config file

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	stdlog "log"
@@ -17,6 +18,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
+	"gopkg.in/yaml.v3"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	kubeinformers "k8s.io/client-go/informers"
@@ -25,9 +28,12 @@ import (
 )
 
 const (
-	defaultListen = ":8080"
+	defaultListen   = ":8080"
+	defaultLogLevel = "info"
 
 	resyncPeriod = time.Minute
+
+	defaultInitNodeCount = 6
 )
 
 var (
@@ -35,8 +41,22 @@ var (
 	kubeconfig string
 	namespace  string
 
-	listen string
+	listen     string
+	logLevel   string
+	configFile string
 )
+
+type config struct {
+	// SyncState consumes a pre-existing ConfigMap if it exists on startup.
+	// Defaults to true.
+	SyncState *bool `yaml:"sync_state"`
+	// WriteInitialState writes a dummy ConfigMap to the filesystem on startup.
+	// Defaults to true. This field is ignored if SyncState is true.
+	WriteInitialState *bool `yaml:"write_initial_state"`
+	// WriteInitialNodeCount is the number of nodes to write to the dummy ConfigMap.
+	// It defaults to 6 to support a replication factor of 5 out of the box
+	WriteInitialNodeCount int `yaml:"write_initial_node_count"`
+}
 
 func main() {
 	flag.Parse()
@@ -57,7 +77,22 @@ func main() {
 		stdlog.Fatalf("error listening on %s: %s", defaultListen, err.Error())
 	}
 
-	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	ctrlCfg := defaultConfig()
+	if configFile != "" {
+		data, err := os.ReadFile(configFile)
+		if err != nil {
+			stdlog.Fatalf("error reading config file: %s", err.Error())
+		}
+		var cfg config
+		if err := yaml.Unmarshal(data, &cfg); err != nil {
+			stdlog.Fatalf("error parsing config file: %s", err.Error())
+		}
+		ctrlCfg = buildConfigFromFile(cfg)
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: getLogLevel(logLevel),
+	}))
 	logger = logger.With("component", "hashring-controller")
 
 	r := prometheus.NewRegistry()
@@ -98,9 +133,10 @@ func main() {
 		r,
 	)
 
-	// todo protect this with a flag
-	if err := controller.EnsureConfigMapExists(ctx); err != nil {
-		stdlog.Fatalf("error ensuring configmap exists: %s", err.Error())
+	if toBool(ctrlCfg.WriteInitialState) {
+		if err := controller.EnsureConfigMapExists(ctx, "receiver", 6); err != nil {
+			stdlog.Fatalf("error ensuring configmap exists: %s", err.Error())
+		}
 	}
 
 	var g run.Group
@@ -121,7 +157,7 @@ func main() {
 			mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 				w.WriteHeader(http.StatusOK)
 			})
-			if err := http.Serve(l, mux); err != nil && err != http.ErrServerClosed {
+			if err := http.Serve(l, mux); err != nil && !errors.Is(err, http.ErrServerClosed) {
 				return fmt.Errorf("server error: %w", err)
 			}
 			return nil
@@ -143,4 +179,56 @@ func init() {
 	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&namespace, "namespace", metav1.NamespaceDefault, "The namespace to watch")
 	flag.StringVar(&listen, "listen", defaultListen, "The address to listen on")
+	flag.StringVar(&logLevel, "log-level", defaultLogLevel, "The log level to use")
+	flag.StringVar(&configFile, "config", "", "Path to a config file")
+
+}
+
+func defaultConfig() config {
+	return config{
+		SyncState:             boolPtr(true),
+		WriteInitialState:     boolPtr(true),
+		WriteInitialNodeCount: defaultInitNodeCount,
+	}
+}
+
+func buildConfigFromFile(cfg config) config {
+	conf := defaultConfig()
+	if cfg.SyncState != nil {
+		conf.SyncState = cfg.SyncState
+	}
+	if cfg.WriteInitialState != nil {
+		conf.WriteInitialState = cfg.WriteInitialState
+	}
+	if cfg.WriteInitialNodeCount != 0 {
+		conf.WriteInitialNodeCount = cfg.WriteInitialNodeCount
+	}
+	return conf
+}
+
+func getLogLevel(lvl string) slog.Level {
+	switch lvl {
+	case "info":
+		return slog.LevelInfo
+	case "debug":
+		return slog.LevelDebug
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		stdlog.Printf("unknown log level %q, defaulting to info", lvl)
+		return slog.LevelInfo
+	}
+}
+
+func boolPtr(b bool) *bool {
+	return &b
+}
+
+func toBool(b *bool) bool {
+	if b == nil {
+		return false
+	}
+	return *b
 }
